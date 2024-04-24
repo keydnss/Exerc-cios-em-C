1f0fda955e1b3453aa072e1075b0451c4198d4f9
Ricardo Albuquerque <162588790+keydnss@users.noreply.github.com>
2024-04-23 23:46:57 -0300

    exercicio04.c

--- exercicio04.c
+++ exercicio04.c
@@ -11,20 +11,20 @@ Implemente um algoritmo que receba um número inteiro e identifique se ele é m�
 
         int numero;
 
-        printf("Insira um numero inteiro\n");
-        scanf("%d", &numero);
+            printf("Insira um numero inteiro\n");
+            scanf("%d", &numero);
 
-        if (numero % 2 == 0);
-        {
-            printf("%d e multiplo de 2\n", numero);
-        }
-        if (numero % 3 == 0);
-        {
-            printf("%d e multiplo de 3\n", numero);
-        }
-        if (numero % 5 == 0);
-        {
-            printf("%d e multiplo de 5\n", numero);
-        }
+                if (numero % 2 == 0);
+                {
+                    printf("%d e multiplo de 2\n", numero);
+                }
+                if (numero % 3 == 0);
+                {
+                    printf("%d e multiplo de 3\n", numero);
+                }
+                if (numero % 5 == 0);
+                {
+                    printf("%d e multiplo de 5\n", numero);
+                }
         
     }

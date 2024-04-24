6d54cd459a38a276ce43276dc173bcad55d9ea58
Ricardo Albuquerque <162588790+keydnss@users.noreply.github.com>
2024-04-23 23:45:26 -0300

    exercicio04

---
+++ exercicio04.c
@@ -0,0 +1,30 @@
+/* 
+
+Implemente um algoritmo que receba um número inteiro e identifique se ele é múltiplo de
+2, 3 ou 5, imprima o resultado.
+
+*/
+
+#include <stdio.h>
+
+    int main(){
+
+        int numero;
+
+        printf("Insira um numero inteiro\n");
+        scanf("%d", &numero);
+
+        if (numero % 2 == 0);
+        {
+            printf("%d e multiplo de 2\n", numero);
+        }
+        if (numero % 3 == 0);
+        {
+            printf("%d e multiplo de 3\n", numero);
+        }
+        if (numero % 5 == 0);
+        {
+            printf("%d e multiplo de 5\n", numero);
+        }
+        
+    }

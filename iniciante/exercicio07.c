b5d5e9a1b8c1171fce1d42744b256e126de27154
Ricardo Albuquerque <162588790+keydnss@users.noreply.github.com>
2024-05-02 19:41:00 -0300

    exercício07

---
+++ iniciante/exercicio07.c
@@ -0,0 +1,16 @@
+/*
+
+7.	Implemente um algoritmo que imprima na ordem inversa uma sequência numérica de 500 a 0, use para isso a estrutura de repetição FOR.
+
+*/
+
+#include <stdio.h>
+
+	int main(){
+	
+		   int i;
+		   
+		   	   for(i=500;i>-1;i--){
+					  printf("%d\n",i);
+				  }	
+	}

1309984aef826868a85e332541305430f84732be
Ricardo Albuquerque <162588790+keydnss@users.noreply.github.com>
2024-05-02 20:58:37 -0300

    exercicio12

---
+++ iniciante/exercicio12.c
@@ -0,0 +1,16 @@
+/*
+	 12. Desenvolva um algoritmo utilizando a estrutura de repetição FOR, para imprimir o alfabeto.
+*/
+
+#include <stdio.h>
+
+	int main(){
+		
+		char letra;
+		
+		for(letra='A';letra<='Z';letra++){
+			
+			printf("%c\n",letra);			
+			
+		}		
+	}  

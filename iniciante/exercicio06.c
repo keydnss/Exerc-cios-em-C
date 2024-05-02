6a6762a58f182e45538ac814311670e2922f4d29
Ricardo Albuquerque <162588790+keydnss@users.noreply.github.com>
2024-05-02 19:33:53 -0300

    exercício 06

---
+++ iniciante/exercicio06.c
@@ -0,0 +1,15 @@
+/* 
+    Implemente um algoritmo que imprima uma sequencia numérica de 0 500, utilizando a estrutura FOR e WHILE.
+*/
+
+#include <stdio.h>
+
+    int main(){
+        
+        int i;
+
+        for(i=0; i<501;	 i++){
+            printf("%d\n", i);
+        }
+
+    }
